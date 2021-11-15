26bd0234dcf6f6268e55382ff1b1fef203e9dad2
indrajeet pande <me@bradtraversy.com>
2021-11-15 19:27:30 +0530

    added other file

---
+++ array_ops.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# use strict;
+# use warnings FATAL => 'all';
+
+@ranks=(1,3);
+@alphabet=(a..z);
+print "all ranks: @ranks \n";
+print"all alphabets: @alphabet\n";
+$size=@alphabet;
+print"size is $size";
